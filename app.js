50a69b3ca7f6a9d343cf6936bd23fef9808ee0b8
Truong Thanh Tin <truongthanhtin6@gmail.com>
2022-04-01 08:41:52 +0700

    [fix] passport authen token

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/login", (req, res, next) => {
 passport.use(
   new LocalStrategy(function (username, password, done) {
     accountModel
-      .findOne({ username: username, password: password })
+      .findOne({ username, password })
       .then((data) => {
         if (!data) done(null, false);
         done(null, data);
@@ -41,20 +41,29 @@ passport.use(
 
 app.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, data) => {
-    if (err) {
-      return res.status(500).json("Lỗi server !");
-    }
-    if (!data) {
-      return res.status(400).json("Sai tài khoản hoặc mật khẩu !");
-    }
+    if (err) return res.status(500).json("Lỗi server !");
+    if (!data) return res.json("Sai tài khoản hoặc mật khẩu !");
 
     jwt.sign(data.toObject(), "123456", (err, token) => {
-      if (err) {
-        return res.status(500).json("Lỗi server !");
-      }
+      if (err) return res.status(500).json("Lỗi server !");
       return res.json(token);
     });
-  });
+  })(req, res, next);
 });
+
+app.get(
+  "/private",
+  (req, res, next) => {
+    const token = req.headers["authorization"].split(" ")[1];
+    jwt.verify(token, "123456", (err, data) => {
+      if (err) return res.status(400).json("Invalid Token !");
+      req.username = data.username;
+      next();
+    });
+  },
+  (req, res, next) => {
+    res.json(`Dữ liệu bí mật ${req.username}`);
+  }
+);
 
 module.exports = app;
